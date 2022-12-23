253cfd18bcccb31e2e9172c1fa4afcfa4ea49d42
WibuSOS <vincent.joyan@gmail.com>
2022-12-23 09:48:14 +0700

    [FIX] magnifier icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
                 <a className="navbar-brand ms-5" href="#id"><img src={ICONS + "profile1.png"} alt={"dd"} style={{ width: "40px", height: "40px", marginRight: "15px" }} /> Bandai.Com</a>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <div style={{ width: "40%", backgroundColor: "white", alignItems: "center", position: "relative", }} className="rounded d-flex ms-4 px-2 py-1">
-                        <img src={ICONS + "search-interface-symbol.png"} alt="search logo" style={{ position: "fixed", width: "25px", height: "25px", }} />
+                        <img src={ICONS + "search-interface-symbol.png"} alt="search logo" style={{ position: "absolute", width: "25px", height: "25px", }} />
                         <input className="w-100 remove-border" style={{ padding: "0 0 0 43px", borderTopStyle: "hidden", borderRightStyle: "hidden", borderStyle: "hidden" }} type="search" placeholder="Search" aria-label="Search" />
                     </div>
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
