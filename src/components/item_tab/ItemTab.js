be02fd6a85681d68c73e55039c55a9ce67d1797e
WibuSOS <vincent.joyan@gmail.com>
2022-12-26 14:44:28 +0700

    [FIX] tab color

--- src/components/item_tab/ItemTab.js
+++ src/components/item_tab/ItemTab.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Row, Tab, Tabs } from 'react-bootstrap'
 
 export const ItemTab = ({ itemList }) => {
+	const [key, setKey] = useState('newest');
+	const [activeClass] = useState('text-dark');
+	const [inactiveClass] = useState('text-active-orange');
+
 	return (
-		<Tabs defaultActiveKey='newest' className='mb-3 mt-3'>
-			<Tab eventKey='newest' title='Newest'>
+		<Tabs activeKey={key} onSelect={k => setKey(k)} className='mb-3 mt-3'>
+			<Tab eventKey='newest' title='Newest' tabClassName={key === 'newest' ? activeClass : inactiveClass}>
 				<Row>
 					{itemList}
 				</Row>
 			</Tab>
-			<Tab eventKey='all' title='All'>
+			<Tab eventKey='all' title='All' tabClassName={key === 'all' ? activeClass : inactiveClass}>
 				<Row>
 					{itemList}
 				</Row>
