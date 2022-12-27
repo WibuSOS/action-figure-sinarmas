2b69dca73f536af73b35272be41d31986e73162e
WibuSOS <vincent.joyan@gmail.com>
2022-12-27 16:28:59 +0700

    [WIP] checkout to history

--- src/const.js
+++ src/const.js
@@ -4,6 +4,7 @@ const LOGIN_URL = `${API_URL}/profiles`;
 const FIGURES_URL = `${API_URL}/figures`;
 const PROFILES_URL = `${API_URL}/profiles`;
 const CART_URL = `${API_URL}/cart`;
+const HISTORY_URL = `${API_URL}/history`;
 
 // DIRECTORIES
 const ASSETS_DIR = "/assets";
@@ -11,11 +12,23 @@ const FIGURES_DIR = `${ASSETS_DIR}/figures`;
 const ICONS = "http://localhost:3000/assets/icons/"
 const IMAGES_CATALOG = "http://localhost:3000/assets/figures/"
 
-const RUPIAH = (number)=>{
-    return new Intl.NumberFormat("id-ID", {
-      style: "currency",
-      currency: "IDR"
-    }).format(number);
+const RUPIAH = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR"
+  }).format(number);
 }
 
-export { API_URL, FIGURES_URL, PROFILES_URL, CART_URL, ASSETS_DIR, FIGURES_DIR, LOGIN_URL, ICONS, IMAGES_CATALOG, RUPIAH }
+export {
+  API_URL,
+  FIGURES_URL,
+  PROFILES_URL,
+  CART_URL,
+  ASSETS_DIR,
+  FIGURES_DIR,
+  LOGIN_URL,
+  ICONS,
+  IMAGES_CATALOG,
+  RUPIAH,
+  HISTORY_URL
+}
